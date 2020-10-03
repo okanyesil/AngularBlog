2a0fe2b0a3d903249e586675b915b29b100496d9
Okan Yeşiloğlu <myesiloglu0@gmail.com>
2020-10-03 22:30:35 +0300

    home ve about-me componentleri eklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutMeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
