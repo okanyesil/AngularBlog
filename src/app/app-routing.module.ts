ee85d88ab38001fdf43a372d6c738c1f0e758d99
Okan Yeşiloğlu <myesiloglu0@gmail.com>
2020-10-03 23:06:25 +0300

    routing Eklendi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutMeComponent} from './about-me/about-me.component';
+import {MainLayoutComponent} from './layout/main-layout/main-layout.component';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+import {AdminLayoutComponent} from './layout/admin-layout/admin-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainLayoutComponent, children: [
+      {path: '', component: HomeComponent},
+      {path: 'hakkimizda', component: AboutMeComponent},
+      {path: 'iletisim', component: ContactComponent}
+    ]},
+  {path: 'admin', component: AdminLayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
