358b2bf8e1dacfa8515fab3f34b3236bf01bde6b
Okan Yeşiloğlu <myesiloglu0@gmail.com>
2020-10-04 12:34:17 +0300

    navbar eklendi

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.css']
 })
 export class MainLayoutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/nav/main-nav/main-nav.component.ts
+++ src/app/nav/main-nav/main-nav.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NavigationEnd, Router} from '@angular/router';
+enum MainPage
+{
+  home = 1,
+  about_me= 2,
+  contanct = 3
+}
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent implements OnInit {
-
-  constructor() { }
+  pageActive: MainPage;
+  constructor(private router: Router) {
+    this.router.events.subscribe(value => {
+      if (value instanceof NavigationEnd) {
+        if (value.url.indexOf('anasayfa') > 0) {
+          this.pageActive = MainPage.home;
+        }
+        else if (value.url.indexOf(('hakkimda')) > 0) {
+          this.pageActive = MainPage.about_me;
+        }
+        else if (value.url.indexOf('iletisim') > 0) {
+          this.pageActive = MainPage.contanct;
+        } else {
+          this.pageActive = MainPage.home;
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
